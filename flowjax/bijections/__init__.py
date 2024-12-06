327dd629ef1a6cb5c918f2682ff2f637b97aed97
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2024-12-06 19:31:12 +0100

    Add Sandwich to __init__

--- flowjax/bijections/__init__.py
+++ flowjax/bijections/__init__.py
@@ -15,7 +15,7 @@ from .rational_quadratic_spline import RationalQuadraticSpline
 from .sigmoid import Sigmoid
 from .softplus import SoftPlus
 from .tanh import LeakyTanh, Tanh
-from .utils import EmbedCondition, Flip, Identity, Invert, Partial, Permute, Reshape
+from .utils import EmbedCondition, Flip, Identity, Invert, Partial, Permute, Reshape, Sandwich
 
 __all__ = [
     "AdditiveCondition",
@@ -39,6 +39,7 @@ __all__ = [
     "Planar",
     "RationalQuadraticSpline",
     "Reshape",
+    "Sandwich",
     "Scale",
     "Scan",
     "Sigmoid",
